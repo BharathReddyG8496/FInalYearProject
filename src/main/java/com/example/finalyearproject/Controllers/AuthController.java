0221fcee383891fe01dc87b84048c855d72fc81c
BharathReddyG8496 <bharath849693@gmail.com>
2025-03-13 15:12:13 +0530

    This is the update after adding the simple controller

---
+++ src/main/java/com/example/finalyearproject/Controllers/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.finalyearproject.Controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
+
+@RestController
+@RequestMapping("/users")
+public class AuthController {
+
+    @GetMapping("/getUser")
+    public ResponseEntity<Object> getUser(){
+        return new ResponseEntity<>("What are you doing,Im a user",HttpStatus.ACCEPTED);
+    }
+
+
+}
