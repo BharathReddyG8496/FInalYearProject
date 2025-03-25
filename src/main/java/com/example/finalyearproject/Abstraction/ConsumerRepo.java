ac99b64ac4bdf793f1b7628f8eb6b02e12c17fb3
jeevanthecoder <jeevanharish897@gmail.com>
2025-03-25 20:07:25 +0530

    Update query solved

--- src/main/java/com/example/finalyearproject/Abstraction/ConsumerRepo.java
+++ src/main/java/com/example/finalyearproject/Abstraction/ConsumerRepo.java
@@ -2,7 +2,9 @@ package com.example.finalyearproject.Abstraction;
 
 import com.example.finalyearproject.DataStore.Consumer;
 import com.example.finalyearproject.DataStore.DeliveryAddresses;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +16,11 @@ import java.util.Set;
 public interface ConsumerRepo extends JpaRepository<Consumer, Integer> {
 
     public Consumer findConsumerByConsumerId(int id);
-//    public Consumer updateByConsumerId(Consumer consumer,int id);
+
+    @Query(value = "update Consumer set consumerFirstName=:#{#consumer.consumerFirstName}, consumerLastName=:#{#consumer.consumerLastName} where consumerId=:#{#id}",nativeQuery = true)
+    public void updateConsumerByconsumerId(Consumer consumer,int id);
+
+
     public Optional<Consumer> findByConsumerName(String consumerName);
 //    public Set<DeliveryAddresses> addDeliv
 }
